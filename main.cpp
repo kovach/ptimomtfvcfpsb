4a7c2f204247e636dd85d6278873d84424e9bd97
Scott Kovach <dskovach@gmail.com>
2016-05-15 13:21:06 -0700

    lol

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// TODO
+// Y: dodge
+// a attack, b grab
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <iostream>
@@ -5,6 +8,35 @@
 #include <cmath>
 
 using namespace std;
+
+
+int maxValuation = 4;
+sf::Color valuationColors[] = {
+    sf::Color::Red,
+    sf::Color(255,128,0,255),
+    sf::Color::Yellow,
+    sf::Color::Green,
+    sf::Color::Green
+};
+
+vector<vector<string> > valuations;
+
+void initValuationNames()
+{
+    for (int i = 0; i < maxValuation+1; i++)
+        valuations.push_back(vector<string>());
+    valuations[0].push_back("ACQUIHIRED");
+    valuations[0].push_back("DREAMS CRUSHED");
+    valuations[1].push_back("REALITY SETS IN");
+    valuations[1].push_back("2-MONTH RUNWAY");
+    valuations[2].push_back("USELESS PRODUCT");
+    valuations[2].push_back("PIVOT");
+    valuations[2].push_back("REFUTED BY SCIENCE");
+    valuations[3].push_back("CTO LEFT");
+    valuations[4].push_back("SERIES A");
+    valuations[4].push_back("YCOMBINATOR");
+}
+    
 
 float strike(float t)
 {
@@ -22,22 +54,61 @@ float sign(float x)
     return -1;
 }
 
-float getJoyX()
+sf::Vector2f normalize(sf::Vector2f v)
 {
-    float v = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+    float r = sqrt(v.x*v.x+v.y*v.y);
+    if (r < 0.05f)
+        return sf::Vector2f(0.f,0.f);
+    return sf::Vector2f(v.x/r, v.y/r);
+}
+
+float clamp(float min, float max, float v)
+{
+    if (v > max) return max;
+    if (v < min) return min;
+    return v;
+}
+
+float getJoyX(int id)
+{
+    float v;
+    if (id == 0)
+        v = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+    else
+        v = sf::Joystick::getAxisPosition(0, sf::Joystick::U);
     v = sign(v) * fmax(0.f, fabs(v)-20.f) * 1.25f;
     return v/100.f;
 }
-float getJoyY()
+float getJoyY(int id)
 {
-    float v = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+    float v;
+    if (id == 0)
+        v = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+    else
+        v = sf::Joystick::getAxisPosition(0, sf::Joystick::V);
     v = sign(v) * fmax(0.f, fabs(v)-20.f) * 1.25f;
     return v/100.f;
 }
 
-bool getJumpButton()
+bool getJumpButton(int id)
 {
+    if (id == 1) return false;
     return sf::Joystick::isButtonPressed(0, 2);
+}
+
+bool getDodgeButton()
+{
+    return sf::Joystick::isButtonPressed(0, 3);
+}
+
+float getAttackButton()
+{
+    return sf::Joystick::isButtonPressed(0, 0);
+}
+
+float getGrabButton()
+{
+    return sf::Joystick::isButtonPressed(0, 1);
 }
 
 enum Direction { None, Left, Right };
@@ -63,6 +134,8 @@ string joystickState()
     for (int c = 0; c < buttons; c++) {
         s << "button " << c << " " << sf::Joystick::isButtonPressed(0, c) << endl;
     }
+    s << "attack " << getAttackButton() << endl;
+    s << "grab " << getGrabButton() << endl;
     return s.str();
 }
 string joystickPositions()
@@ -90,10 +163,22 @@ enum LungeState {
 string LungeStates[] = {
     "Ready", "Striking_Left", "Striking_Right", "Recovery"
 };
-// TODO needed?
-enum JumpEdge {
-    StopJump, StartJump, NoEdge
+enum AttackState {
+    AS_Ready, AS_Striking, AS_Grabbing
 };
+string AttackStates[] = {
+    "AS_Ready", "AS_Striking", "AS_Grabbing"
+};
+enum MotionState {
+    MS_Free, MS_Struck
+};
+string MotionStates[] = {
+    "MS_Free", "MS_Struck"
+};
+// todo needed?
+//enum JumpEdge {
+//    StopJump, StartJump, NoEdge
+//};
 
 const int fps = 60;
 const float groundLevel = 750.;
@@ -101,38 +186,71 @@ const float groundLevel = 750.;
 class ControlState
 {
 public:
+    int id;
     JumpState jump;
     LungeState lunge;
-    int facing;
+    AttackState attack;
+    MotionState motion;
 
+    int facing; // TODO
+
+    // Main state
     sf::Vector2f p;
     sf::Vector2f v;
     float radius;
+    int valuation;
+    string valuationLabel;
 
+    sf::Vector2f arm;
+
+    // State machine timing state
     int crouchDuration;
     int lungeDuration;
+    int attackDuration;
+    int struckDuration;
+    sf::Vector2f attackStart;
+    sf::Vector2f attackEnd;
+    // State machine parameters
     const static float maxCrouch = fps/10;
     const static float maxLunge = fps/12;
     const static float maxRecovery = fps/6;
+    const static float maxAttack = fps*5/6;
+    const static float maxStruck = fps/4;
 
+    // General Parameters
     const static float maxVertical = 5000;
     const static float maxHorizontal = 3500;
+    const static float maxDodgeHorizontal = 7000;
     const static float gravity = 15000.f;
-    const static float jumpForce = 19000.f;
+    const static float jumpForce = 25000.f;
+    const static float attackRange = 800.f;
+    const static float strikeForce = 1820.f;
+
 
     ControlState()
     {
         resetJump();
         jump = Floating;
         resetLunge();
+        resetAttack();
+        resetMotion();
+
         radius = 100.;
         p.x = 0.f;
         p.y = 0.f;
         v.x = 0.f;
         v.y = 0.f;
-
+        valuation = maxValuation;
+        updateValuationLabel();
     }
 
+    void initX(int max)
+    {
+        if (id == 0)
+            p.x = 100.f + radius;
+        else
+            p.x = max - 100.f - radius;
+    }
     void resetJump()
     {
         jump = Ground;
@@ -143,11 +261,37 @@ public:
         lunge = Ready;
         lungeDuration = 0;
     }
+    void resetAttack()
+    {
+        attack = AS_Ready;
+        attackDuration = 0;
+    }
+    void resetMotion()
+    {
+        motion = MS_Free;
+        struckDuration = 0;
+    }
+
+    void initiateAttack()
+    {
+        sf::Vector2f vec(getJoyX(id), getJoyY(id));
+        float r = sqrt(vec.x*vec.x+vec.y*vec.y);
+        attackStart = p;
+        if (r < 0.05f)
+            attackEnd = attackStart;
+        else
+            attackEnd = attackStart + vec / r * attackRange;
+    }
+
+    sf::Vector2f getCenter()
+    {
+        return p;
+    }
 
     void stateUpdate()
     {
         // Update jump
-        if (getJumpButton()) {
+        if (getJumpButton(id)) {
             if (jump == Ground) {
                 jump = Crouching;
                 crouchDuration = 0;
@@ -161,7 +305,7 @@ public:
         }
         crouchDuration++;
 
-        // Update attack
+        // Update lunge
         Direction action = getLungeButton();
         switch (lunge) {
             case Ready:
@@ -182,87 +326,203 @@ public:
                 break;
         }
         lungeDuration++;
+
+        // Update attack
+        switch (attack) {
+            case AS_Ready:
+                if (getAttackButton()) {
+                    attack = AS_Striking;
+                }
+                if (getGrabButton()) {
+                    attack = AS_Grabbing;
+                }
+                initiateAttack();
+                attackDuration = 0;
+                arm = getCenter();
+                break;
+            case AS_Striking:
+            case AS_Grabbing:
+                if (attackDuration > maxAttack)
+                    resetAttack();
+                else {
+                    float t = (float)attackDuration / maxAttack;
+                    float r = strike(t);
+                    sf::Vector2f p1 = attackStart * (1.f - r) + attackEnd * r;
+                    sf::Vector2f p2 = p1 * (1.f - t) + p * t;
+                    arm = p2;
+                }
+                break;
+        }
+        attackDuration++;
+
+        switch (motion) {
+            case MS_Free:
+                struckDuration = 0;
+                break;
+            case MS_Struck:
+                if (struckDuration++ > maxStruck) {
+                    resetMotion();
+                }
+                break;
+        }
+
     }
+    void updateVelocity(float dt)
+    {
+        sf::Vector2f newV = v;
+        // Horizontal joystick
+        float dvx = getJoyX(id) * 500.f;
+        if (jump == Crouching || jump == Floating)
+            dvx /= 3;
+        if (motion == MS_Struck)
+            dvx /= 3;
+
+        newV.x += dvx;
+        if (motion == MS_Free)
+            newV.x = clamp (-maxHorizontal, maxHorizontal, newV.x);
+
+        if (motion == MS_Free) {
+            switch(jump) {
+                //case Ground:
+                //    if (lunge == Ready) {
+                //        //newV.x = getJoyX() * 2500;
+                //        facing = sign(newV.x);
+                //    }
+                //    //vy -= gravity * dt;
+                //    newV.y = 0.;
+                //    break;
+                case Crouching:
+                    newV.y += jumpForce * dt;
+                    break;
+            }
+        }
+
+        newV = applyGravity(newV, dt);
+
+        switch(lunge) {
+            case Ready:
+                break;
+            case Striking_Left:
+                if (motion == MS_Free)
+                    newV.x = -maxDodgeHorizontal * strike((float)lungeDuration/maxRecovery);
+                break;
+            case Striking_Right:
+                if (motion == MS_Free)
+                    newV.x = maxDodgeHorizontal * strike((float)lungeDuration/maxRecovery);
+                break;
+            case Recovery:
+                //vx = sign(vx) * maxHorizontal * strike(lungeDuration/maxRecovery);
+                break;
+        }
+
+        switch (motion) {
+            case MS_Free:
+                v = newV;
+                v.x *= 0.9; // todo
+                break;
+            case MS_Struck:
+                v = newV;
+                break;
+        }
+    }
+
+    sf::Vector2f applyGravity(sf::Vector2f v, float dt)
+    {
+        if ((jump == Floating || jump == Ground) && motion == MS_Free)
+            v.y -= gravity * dt;
+        if (lunge == Striking_Right || lunge == Striking_Left)
+            v.y = 0.;
+        return v;
+    }
+
     void update(float dt)
     {
         stateUpdate();
-        getVelocity(dt);
+        updateVelocity(dt);
         p.x += dt * v.x;
         p.y -= dt * v.y;
 
         if (p.y > groundLevel) {
             resetJump();
             p.y = groundLevel;
+            v.y = 0;
         }
 
     }
 
-    void getVelocity(float dt)
-    {
-        switch(jump) {
-            case Ground:
-                if (lunge == Ready) {
-                    v.x = getJoyX() * 2500;
-                    facing = sign(v.x);
-                }
-                //vy -= gravity * dt;
-                v.y = 0.;
-                break;
-            case Crouching:
-                v.y += jumpForce * dt;
-                //vy = maxVertical * strike((float)crouchDuration/maxCrouch);
-                break;
-            case Floating:
-                float g = gravity;
-                v.y -= g * dt;
-                if (lunge == Striking_Right || lunge == Striking_Left)
-                    v.y = 0.;
-                break;
-        }
-
-        switch(lunge) {
-            case Ready:
-                break;
-            case Striking_Left:
-                v.x = -maxHorizontal * strike((float)lungeDuration/maxRecovery);
-                break;
-            case Striking_Right:
-                v.x = maxHorizontal * strike((float)lungeDuration/maxRecovery);
-                break;
-            case Recovery:
-                //vx = sign(vx) * maxHorizontal * strike(lungeDuration/maxRecovery);
-                break;
-        }
-    }
 
     string toString(float time)
     {
         std::ostringstream s;
-        s << JumpStates[jump] << endl << LungeStates[lunge];
+        s << JumpStates[jump] << endl << LungeStates[lunge] << endl
+            << AttackStates[attack] << endl
+            << MotionStates[motion] << endl;
         return s.str();
+    }
+
+    void updateValuationLabel()
+    {
+        valuationLabel = valuations[valuation][rand() % valuations[valuation].size()];
+    }
+    void doHit(ControlState &target)
+    {
+        if (target.motion == MS_Struck)
+            return;
+        sf::Vector2f v = arm - p;
+        v = normalize(v) * strikeForce;
+        v.y = -v.y;
+        switch(attack) {
+            case AS_Ready:
+                return;
+            case AS_Striking:
+                v += sf::Vector2f(0, 1200.f);
+                break;
+            case AS_Grabbing:
+                v = -v;
+                break;
+        }
+        target.motion = MS_Struck;
+        target.valuation--;
+        target.updateValuationLabel();
+        if (target.valuation < 0) target.valuation = 0;
+        target.v = v;
     }
 };
 
-bool collision(ControlState s1, ControlState s2)
+bool collision(ControlState &s1, ControlState &s2)
 {
-    sf::Vector2f v = s1.p - s2.p;
-    if (v.x*v.x + v.y*v.y < (s1.radius + s2.radius)^2) {
-        return true;
+    sf::Vector2f v;
+    // P2 hits P1
+    v = s1.p - s2.arm;
+    if (v.x*v.x + v.y*v.y < s1.radius*s1.radius)
+    {
+        s2.doHit(s1);
+    }
+    // P1 hits P2
+    v = s2.p - s1.arm;
+    if (v.x*v.x + v.y*v.y < s2.radius*s2.radius)
+    {
+        s1.doHit(s2);
     }
     return false;
 }
 
-class Thing : public sf::Drawable
+class Char : public sf::Drawable
 {
 public:
-    Thing()
+    sf::Vector2u windowDim;
+
+    Char(int myid, sf::Vector2u dim, bool showText)
     {
+        controller.id = myid;
+
+        // Graphics state
         if (!s_font.loadFromFile("noto.ttf"))
             exit(22);
         m_text.setFont(s_font);
-        m_text.setPosition(0.f, 0.f);
         m_text.setCharacterSize(36);
-        m_text.setColor(sf::Color(255,128,0,255));
+        m_text.setColor(sf::Color(0,255,0,255));
+        windowDim = dim;
 
         if (!m_texture.loadFromFile("pt-crop.jpg"))
             exit(23);
@@ -270,17 +530,30 @@ public:
         m_sprite.setScale(0.25f, 0.25f);
         m_sprite.setPosition(300, 300);
 
+        // Game state
+        controller.initX(dim.x);
     }
-    virtual ~Thing()
+    virtual ~Char()
     {
     }
     void update(float time)
     {
         float dt = 1.0/60;
 
-        controlState.update(dt);
+        controller.update(dt);
 
-        m_sprite.setPosition(controlState.p.x, controlState.p.y);
+        m_sprite.setPosition(controller.p.x, controller.p.y);
+
+        m_text.setColor(valuationColors[controller.valuation]);
+        m_text.setString(controller.valuationLabel);
+        sf::FloatRect bounds = m_text.getLocalBounds();
+        float x;
+        if (controller.id == 0)
+            x = 70;
+        else
+            x = windowDim.x - bounds.width - 70;
+        m_text.setPosition(x, windowDim.y-150.f);
+        
 
         std::ostringstream s;
         if (dt > 0.018f) {
@@ -290,18 +563,44 @@ public:
         s   << 1./dt << endl
             << joystickState() << endl
             << state() << endl
-            << controlState.toString(time) << endl;
+            << controller.toString(time) << endl;
 
-        m_text.setString(s.str());
+        //m_text.setString(s.str());
+    }
+    void drawArm(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        float x = controller.arm.x;
+        float y = controller.arm.y;
+        float r = 22.f;
+        sf::CircleShape shape(r);
+        if (controller.attack == AS_Ready)
+            shape.setFillColor(sf::Color(0,204,102,255));
+        else if (controller.attack == AS_Striking)
+            shape.setFillColor(sf::Color(255,0,0,255));
+        else if (controller.attack == AS_Grabbing)
+            shape.setFillColor(sf::Color(255,128,0,255));
+        shape.setPosition(x-r, y-r);
+        target.draw(shape);
+    }
+    void drawStatus(sf::RenderTarget& target, sf::RenderStates states) const
+    {
     }
     void draw(sf::RenderTarget& target, sf::RenderStates states) const
     {
         target.draw(m_text, states);
-        target.draw(m_sprite);
+        //target.draw(m_sprite);
+        float x = controller.p.x;
+        float y = controller.p.y;
+        float r = controller.radius;
+        sf::CircleShape shape(r);
+        shape.setFillColor(sf::Color(0,222,0,128));
+        shape.setPosition(x-r, y-r);
+        target.draw(shape);
+
+        drawArm(target, states);
     }
 
-private:
-    ControlState controlState;
+    ControlState controller;
 
     sf::Font s_font;
     sf::Text m_text;
@@ -313,8 +612,10 @@ private:
     {
         std::ostringstream s;
         s
-            << "x: " << controlState.p.x << endl
-            << "y: " << controlState.p.y << endl;
+            << "x: " << controller.p.x << endl
+            << "y: " << controller.p.y << endl
+            << "vx: " << controller.v.x << endl
+            << "vy: " << controller.v.y << endl;
         return s.str();
     }
 
@@ -325,12 +626,23 @@ private:
     //}
 };
 
+void mainUpdate(Char &t1, Char &t2)
+{
+    t1.update(0);
+    t2.update(0);
+    // updateoverlay
+    collision(t1.controller, t2.controller);
+}
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    srand(time(0));
 
     //sf::CircleShape shape(100.f);
     //shape.setFillColor(sf::Color::Green);
+
+    initValuationNames();
 
     sf::Vector2u dim = window.getSize();
 
@@ -347,7 +659,9 @@ int main()
 
     window.setFramerateLimit(fps);
 
-    Thing thing;
+    float offset = 200.f;
+    Char thing(0, dim, true);
+    Char thing2(1, dim, false);
 
     sf::Clock clock;
     thing.update(clock.getElapsedTime().asSeconds());
@@ -361,13 +675,14 @@ int main()
                 window.close();
 
         }
-        thing.update(0);
+        mainUpdate(thing, thing2);
         //clock.getElapsedTime().asSeconds();
 
         window.clear();
         window.draw(thing);
-        window.draw(ground);
-        window.display();
+        window.draw(thing2);
+        //window.draw(ground);
+        window.display(); // this tries to enforce the frame limit
     }
     window.close();
     return 0;
